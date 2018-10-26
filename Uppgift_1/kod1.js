1176b14f39023b8c6d7e260ecbc1657d351165bd
fridaschoultz <fridaschoultz@hotmail.com>
2018-10-26 12:54:20 +0200

    Ändringar

--- Uppgift_1/kod1.js
+++ Uppgift_1/kod1.js
@@ -1,7 +1,6 @@
- // En konstruktor som skapar två olika objekt som blir en blueprint för att beskriva två personer
+ // Konstruktor som skapar en blueprint för att beskriva två personer
 
-	  function Person(name, epost, mobil, adress){ /* Funktionen Person blir en egen "ritning"
-                                                        som beskriver en persons egenskaper. Person blir en ny prototyp */
+	  function Person(name, epost, mobil, adress){ /* Funktionen Person innehåller egenskaper. */
 			this.name = name;
 			this.epost  = epost;
 			this.mobil = mobil;
@@ -10,13 +9,10 @@
 				console.log(this.name +"\n\n"+
 				this.adress.gatuadress +"\n"+
 				this.adress.postnr +" "+
-				this.adress.ort);          /*metoden this och egenskapen print blir en
-							    funktion som utför uppgiften att skriva ut 
-							    önskad information till terminalen, genom att
-							    referera (this) till objektet Person där egenskapen 
-							    adress finns och sedan dot notation för respektive
-							    egenskap (gatuadress,postnr,ort) man vill använda. 
-							    Här väljer man vad som skall visas i anropen print() längst ner  */
+				this.adress.ort);          /* this.print() blir en
+							               funktion som utför uppgiften att skriva ut 
+										   önskad information. Här väljer man 
+										   vad som skall visas i anropen print() längst ner */
 			} 
 
 		}
@@ -25,29 +21,21 @@
 		function Adress (gatuadress, postnr, ort) {
 			this.gatuadress = gatuadress;
 			this.postnr = postnr;
-			this.ort = ort;    /* Funktionen Adress skapar en "ritning" där Adress får ytterligare egenskaper
+			this.ort = ort;    /* Funktionen Adress innehåller ytterligare egenskaper
 					            för att beskriva personen/personerna. */
 	}
 	
-	// adress1 och adress2 blir objekt (instanser) av typen Adress som har tre parametrar.
-	//frida och isabelle blir objekt (instanser) av typen Person som har fyra parametrar.
+	//Här skapas två olika objekt (instanser) som skickar inargument till respektive objekts parametrar.
 	 
-	let adress1 = new Adress ("Björnspinnarvägen 113", "14137", "Huddinge"); /* Nytt objekt skapas och inargument skickas till 
-	                                                                          Adress() och dess parametrar. */
-	let frida = new Person ("Frida Schoultz", "fridaschoultz@hotmail.com", "0736781423", adress1); /* Nytt objekt skapas och 
-	                                                                                                inargument + adress1 
-												                                                    skickas till Person() och dess 
-												                                                    parametrar. */
+	let adress1 = new Adress ("Björnspinnarvägen 113", "14137", "Huddinge"); 
+	let frida = new Person ("Frida Schoultz", "fridaschoultz@hotmail.com", "0736781423", adress1); 
 
-	let adress2 = new Adress ("Kyrkbacken 6", "12364", "Solna"); /* Nytt objekt skapas och inargument skickas till Adress och dess 
-                                                                      parametrar()*/
-	let isabelle = new Person ("Isabelle Schoultz", "blabla@hotmail.com", "0704561738", adress2); /*Här skapas nytt objekt och 
-	                                                                                               inargument + adress2 
-												                                                   skickas till Person() och dess 
-												                                                   parametrar. */
+	let adress2 = new Adress ("Kyrkbacken 6", "12364", "Solna"); 
+	let isabelle = new Person ("Isabelle Schoultz", "blabla@hotmail.com", "0704561738", adress2); 
 
-	isabelle.print(); // Anropt till print() som finns i funktionen Person() som skriver ut den aktuella informationen om objektet isabelle
-	frida.print(); // Anropt till print() som finns i funktionen Person() som skriver ut den aktuella informationen om objektet frida.
+	// Anropar och visar aktuell information
+	isabelle.print(); 
+	frida.print(); 
 		
 
 		
